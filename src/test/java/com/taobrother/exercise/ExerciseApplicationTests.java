29dd648a34e746fa40e3448cc393e6042a08eecf
Jingtao Zhou <jingtao.zhou@thoughtworks.com>
2019-11-28 22:28:56 +0800

    init project

---
+++ src/test/java/com/taobrother/exercise/ExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taobrother.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class ExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
