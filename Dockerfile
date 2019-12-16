6b3ecdb9a307fdfd0fad79284f95cdd54e028bb7
Jingtao Zhou <jingtao.zhou@thoughtworks.com>
2019-12-16 14:19:39 +0800

    change application port

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ FROM openjdk:8u151-jdk-slim
 COPY ./build/libs/*.jar /app/
 COPY ./run.sh /app/
 
-EXPOSE 2333
+EXPOSE 8888
 
 CMD ["/app/run.sh"]
--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ docker stop exercise || true
 docker rm exercise || true
 
 echo "run exercise"
-docker run --name exercise -d -p 8888:2333 --network=host exercise:latest
+docker run --name exercise -d --network=host exercise:latest
