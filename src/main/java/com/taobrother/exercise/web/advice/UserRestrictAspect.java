5917e9b3960035ffaa068e97d99c374e97c1e9d8
Jingtao Zhou <jingtao.zhou@thoughtworks.com>
2020-01-18 17:48:26 +0800

    change the aspect args type

--- src/main/java/com/taobrother/exercise/web/advice/UserRestrictAspect.java
+++ src/main/java/com/taobrother/exercise/web/advice/UserRestrictAspect.java
@@ -1,6 +1,7 @@
 package com.taobrother.exercise.web.advice;
 
 import com.taobrother.exercise.web.exception.InvalidUserException;
+import com.taobrother.exercise.web.request.SignUpRequest;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -9,9 +10,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserRestrictAspect {
 
-    @Before("@annotation(com.taobrother.exercise.web.annotation.VerifyName) && args(name, ..)")
-    public void validateUserName(String name) {
-        if(name == null || name.length()==0){
+    @Before("@annotation(com.taobrother.exercise.web.annotation.VerifyName) && args(request, ..)")
+    public void validateUserName(SignUpRequest request) {
+        if(request.getName() == null || request.getName().length()==0){
             throw new InvalidUserException("user name is not valid");
         }
     }
