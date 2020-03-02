685cebda2d1ed149b3603a4aa5a17d75bf24a663
Jingtao Zhou <jingtao.zhou@thoughtworks.com>
2020-03-02 11:57:24 +0800

    change user

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,4 @@ echo "delete old images"
 docker rmi $(docker images | grep "none" | awk '{print $3}') || true
 
 echo "run exercise"
-docker run --name exercise -v /home/campaign-admin/:/test -p 2333:8888 -d exercise:latest
+docker run --name exercise -u campaign-admin -g staff -v /home/campaign-admin/:/test -p 2333:8888 -d exercise:latest
